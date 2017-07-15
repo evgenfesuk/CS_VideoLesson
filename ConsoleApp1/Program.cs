0bda961d0390a42f5212b6e45677fc0cfd180369
evgen.fesuk <evgen.fesuk@gmail.com>
2017-07-15 18:49:21 +0300

    creating directory and writing to file

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        
+        private static string dirPath = "D:\\test";
+        private static string fileName = dirPath + "\\test.txt";
         static void Main(string[] args)
         {
+            if(!Directory.Exists(dirPath)) Directory.CreateDirectory(dirPath);
+            File.AppendAllText(fileName, "some text\r\n");
         }
     }
 }
