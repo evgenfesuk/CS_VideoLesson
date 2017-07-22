00369a9d53c3c2513f7ab011a2d702ff437ddb94
evgen.fesuk <evgen.fesuk@gmail.com>
2017-07-22 19:04:52 +0300

    ready timer using delegates

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,53 +18,32 @@ namespace ConsoleApp1
             BooleanDelegate IsPositive = number.IsPositive;
             BooleanDelegate IsZero = number.IsZero;
 
-            // таймер
-            Timer time = new Timer();
 
-            // делегат для запуска таймера
-            TimerDelegate InitTimer = time.InitTimer;
 
             // переменная для ввода кол-ва секунд для таймера
-            int tmpTime;
+            int tmpTime = 0;
 
             // ввод с консоли
-            Console.Write("Input seconds: ");
+            Console.Write("Input seconds: \n");
 
             // пытаемся конвертировать строку в число
-            if(!(int.TryParse(Console.ReadLine(), out tmpTime))) Console.WriteLine("Can not parse to integer"); ;
+            if(!(int.TryParse(Console.ReadLine(), out tmpTime))) Console.WriteLine("Can not parse to integer");
 
-            // если введенное число >=0 - запускаем таймер
-            // вариант 1
-            /*if (UseDelegate(IsPositive, tmpTime) || UseDelegate(IsZero, tmpTime)) UseDelegate(InitTimer, tmpTime); 
-            else Console.WriteLine("Time can not be negative");*/
+            // если введенное число >=0 - инициализируем создание и запуск таймера
+            if (IsPositive(tmpTime) || IsZero(tmpTime))
+            {
+                // создаём таймер
+                Timer time = new Timer(tmpTime);
 
-            // вариант 2
-            if (IsPositive(tmpTime) || IsZero(tmpTime)) InitTimer(tmpTime);
+                // выбираем способ вывода на экран
+                ShowDelegate show = time.ShowNoLine;
+
+                // запускаем таймер с выбраным способом вывода на экран
+                time.InitTimer(show);
+            }
             else Console.WriteLine("Time can not be negative");
 
             Console.ReadKey();
-        }
-
-        /// <summary>
-        /// Метод для работы с делегатами по проверке числа на >0, <0, ==0
-        /// </summary>
-        /// <param name="method">Передаваемый метод</param>
-        /// <param name="num">Проверяемое число</param>
-        /// <returns></returns>
-        static bool UseDelegate(BooleanDelegate method, int num)
-        {
-            return method(num);
-        }
-
-
-        /// <summary>
-        /// Метод для работы с делегатами для работы с таймером
-        /// </summary>
-        /// <param name="method">Передаваемый метод</param>
-        /// <param name="time">Время в секундах</param>
-        static void UseDelegate(TimerDelegate method, int time)
-        {
-            method(time);
         }
     }
 }
--- ConsoleApp1/Timer.cs
+++ ConsoleApp1/Timer.cs
@@ -7,13 +7,12 @@ using System.Threading;
 
 namespace ConsoleApp1
 {
-
     /// <summary>
-    /// Делегат для таймера
+    /// Какой-то способ вывода на экран результата работы таймера
     /// </summary>
-    /// <param name="time"></param>
-    public delegate void TimerDelegate(int time);
-
+    /// <param name="i">Текущее состояние таймера</param>
+    /// <param name="timer">Финальное значение таймера</param>
+    public delegate void ShowDelegate(int i, int timer);
 
     /// <summary>
     /// Класс Timer
@@ -33,24 +32,60 @@ namespace ConsoleApp1
         }
 
         /// <summary>
-        /// Запуск таймера с заданым временем
+        /// Вывод на экран построчно
+        /// </summary>
+        /// <param name="i">Текущее состояние таймера</param>
+        /// <param name="timer">Финальное значение таймера</param>
+        public void ShowLine(int i, int timer)
+        {
+            Console.Clear(); // очистка строки
+            Console.WriteLine("Timer for {0} seconds started", timer); // вывод сообщения о величине таймера
+            if (i-1 > 0) // если 1 секунда уже выведена на экран
+            {
+                for (int j=1; j<i-1; j++) // если это вторая секунда - идём мимо
+                Console.WriteLine(j); // если это 3 и больше секунда - заново выводим первую секунду на экран
+
+                Console.WriteLine(i - 1); // выводим значение текущего таймера-1
+            }
+            Console.WriteLine(i); // выводим значение текущего таймера
+            if (i == timer) Console.Beep(); // звуковое оповещение по окончании
+            Thread.Sleep(1000);
+        }
+
+        /// <summary>
+        /// Вывод на экран в одну строку
+        /// </summary>
+        /// <param name="i">Текущее состояние таймера</param>
+        /// <param name="timer">Финальное значение таймера</param>
+        public void ShowNoLine(int i, int timer)
+        {
+            Console.Clear(); // очистка строки
+            Console.WriteLine("Timer for {0} seconds started", timer); // вывод сообщения о величине таймера
+            Console.Write(i); // вывод таймера
+            if (i == timer) Console.Beep(); // звуковое оповещение по окончании
+            Thread.Sleep(1000); // ждём 1 сек
+        }
+
+        /// <summary>
+        /// Запуск таймера с заданым временем и делегатом вывода на экран
         /// </summary>
         /// <param name="time">Время в секундах</param>
-        public void InitTimer(int time)
+        /// <param name="show">Один из вариантов вывода на экран</param>
+        public void InitTimer(int time, ShowDelegate show)
         {
             for (int i = 1; i <= time; i++)
             {
-                Console.WriteLine(i);
-                Thread.Sleep(1000);
+                show(i, time);
             }
         }
 
         /// <summary>
-        /// Запуск таймера без явного указания вреемни
+        /// Запуск таймера без явного указания времени, но с делегатом вывода на экран
         /// </summary>
-        public void InitTimer()
+        /// <param name="show">Один из вариантов вывода на экран</param>
+        public void InitTimer(ShowDelegate show)
         {
-            InitTimer(Time);
+            InitTimer(Time, show);
         }
 
         /// <summary>
